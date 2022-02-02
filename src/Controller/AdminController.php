f158a737105516586db402b54b0e10b321ce042c
Nibor51 <jonval.robin@gmail.com>
2022-02-02 20:31:10 +0100

    added login and logout

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,14 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'admin')]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'last_username' => $lastUsername,
+            'error' => $error,
         ]);
     }
 }
