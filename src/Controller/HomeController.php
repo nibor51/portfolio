f98e88b85886eb5315de1a91a78e06515ed26ed3
Nibor51 <jonval.robin@gmail.com>
2022-02-02 18:20:11 +0100

    created navbar(html and css) and include

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\WorkRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/works', name: 'works')]
+    public function works(WorkRepository $work): Response
+    {
+        return $this->render('home/works.html.twig', [
+            'works' => $work->findAll(),
         ]);
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('home/contact.html.twig');
     }
 }
