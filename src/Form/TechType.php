73e31790d170351c103a177fcd2016590a3a7a03
Nibor51 <jonval.robin@gmail.com>
2022-02-03 14:42:30 +0100

    added Types in TechType

--- src/Form/TechType.php
+++ src/Form/TechType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Tech;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,8 @@ class TechType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('logo')
+            ->add('name', TextType::class)
+            ->add('logo', TextType::class)
         ;
     }
 
