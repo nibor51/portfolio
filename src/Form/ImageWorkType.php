ec7b8af71f1f64ea57483f254f8c176488c09d97
Nibor51 <jonval.robin@gmail.com>
2022-02-03 14:42:15 +0100

    added Types in ImageWorlType

--- src/Form/ImageWorkType.php
+++ src/Form/ImageWorkType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\ImageWork;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,12 @@ class ImageWorkType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('alt')
-            ->add('src')
-            ->add('work')
+            ->add('alt', TextType::class)
+            ->add('src', TextType::class)
+            ->add('work', EntityType::class, [
+                'class' => 'App\Entity\Work',
+                'choice_label' => 'title',
+            ])
         ;
     }
 
